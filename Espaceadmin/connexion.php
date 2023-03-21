06d664262755b0cf3a57ab2b854f3c9f8459ada8
RomeoMokabo <98831407+RomeoMokabo@users.noreply.github.com>
2023-03-21 12:01:30 +0100

    Add files via upload

---
+++ Espaceadmin/connexion.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+if(isset($_POST['valider'])){
+   if(!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){
+      $pseudo_par_defaut = "admin";
+      $mdp_par_defaut = "admin1234";
+
+      $pseudo_saisi = htmlspecialchars($_POST['pseudo']);
+      $mdp_saisi = htmlspecialchars($_POST['mdp']);
+
+      if($pseudo_saisi == $pseudo_par_defaut AND $mdp_saisi == $mdp_par_defaut){
+         $_SESSION['mdp'] = $mdp_saisi;
+         header('Location: index.php');
+      }else{
+         echo "votre mode de passe ou pseudo est incorrect";
+      }   
+   }else{
+      echo "Veuillez compléter tous les champs...";
+   }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <form method="POST" action="" align="center">
+      <input type="text" name="pseudo" autocomplete="off">
+      <br>
+      <input type="password" name="mdp">
+      <br><br>
+      <input type="submit" name="valider">
+   </form>
+</body>
+</html>
+
